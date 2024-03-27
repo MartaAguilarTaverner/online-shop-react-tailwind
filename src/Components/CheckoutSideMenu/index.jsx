ef9c703a1cb97c37bbe9befd38656f3fb8bec102
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2024-03-27 17:56:03 +0100

    added the component orderCard in the checkout menu for each product added in cart, added scroll in the right side to be able to see all the products, and changes in the styles

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,20 +1,34 @@
 import { XCircleIcon } from '@heroicons/react/24/solid';
+
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context';
+
+import OrderCard from '../OrderCard';
+
 import './checkoutSideMenu.css';
 
 
 const CheckoutSideMenu = () => {
 const context = useContext(ShoppingCartContext);
 
+console.log('Cart: ', context.shoppingCartProducts);
+
   return (
-    <aside className={` ${context.checkoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex flex-col fixed right-0 mr-4 border border-purple-300 rounded-lg bg-white`}>
+    <aside className={` ${context.checkoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu scrollable-cards flex flex-col fixed right-0 mr-4 border border-purple-300 rounded-lg bg-white`}>
       <div className='flex justify-between items-center mt-2 mr-3 ml-3'>
         <h2 className='font-medium text-xl'>My Order</h2>
         <button className='cursor-pointer' onClick={() => context.closeCheckoutSideMenu()}>
           <XCircleIcon className='h-7 w-7 text-violet-700'></XCircleIcon>
         </button>
       </div>
+      <div>
+      {
+        context.shoppingCartProducts.map( product => (
+          <OrderCard key={product.id} title={product.title} image={product.image} price={product.price} />
+        ))
+      }
+      </div>
+
     </aside>
   )
 };

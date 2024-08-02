4de3003a5ceb5165ad003498b443d5f8223116eb
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2024-08-02 13:47:13 +0200

    changes to be able to delete a product in the order list(checkout menu) and to be able to show the total price of an order

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -3,6 +3,8 @@ import { XCircleIcon } from '@heroicons/react/24/solid';
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context';
 
+import { totalPrice } from '../../Utils';
+
 import OrderCard from '../OrderCard';
 
 import './checkoutSideMenu.css';
@@ -11,7 +13,10 @@ import './checkoutSideMenu.css';
 const CheckoutSideMenu = () => {
 const context = useContext(ShoppingCartContext);
 
-console.log('Cart: ', context.shoppingCartProducts);
+const handleDelete = (id) => {
+  const filteredProducts = context.shoppingCartProducts.filter(product => product.id !== id)
+  context.setShoppingCartProducts(filteredProducts)
+}
 
   return (
     <aside className={` ${context.checkoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu scrollable-cards flex flex-col fixed right-0 mr-4 border border-purple-300 rounded-lg bg-white`}>
@@ -24,9 +29,17 @@ console.log('Cart: ', context.shoppingCartProducts);
       <div>
       {
         context.shoppingCartProducts.map( product => (
-          <OrderCard key={product.id} title={product.title} image={product.image} price={product.price} />
+          <OrderCard key={product.id} id={product.id} title={product.title} image={product.image} price={product.price} handleDelete={handleDelete} />
         ))
       }
+      </div >
+      <div className='divide-y-4 divide-double divide-purple-900'>
+        <div className='mt-5 mb-10 mr-3 ml-3'>
+          <p className='flex justify-between items-center ml-10 mr-2'>
+            <span className='font-semibold text-lg'>Total</span>
+            <span className='font-bold text-xl'>${totalPrice(context.shoppingCartProducts)} </span>
+          </p>
+        </div>
       </div>
 
     </aside>

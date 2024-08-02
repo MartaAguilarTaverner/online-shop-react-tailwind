b99839976f2070154a6ed0ef034259fb515a08bc
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2024-08-02 13:48:38 +0200

    changes in the order card to be able to implement the functionality of deleting products in the checkoutsidemenu

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -4,7 +4,7 @@ import { XCircleIcon } from '@heroicons/react/24/solid';
 import PropTypes from 'prop-types';
 
 
-const OrderCard = ({ image, title, price }) => {
+const OrderCard = ({ id, image, title, price, handleDelete }) => {
   return(
     <div className="flex justify-between items-center mt-4 mb-4 mr-4 ml-4" >
       <div className=" flex flex-col item-center ">
@@ -15,16 +15,18 @@ const OrderCard = ({ image, title, price }) => {
       </div>
       <div className="flex items-center ">
         <span className="text-lg font-medium mr-1">{price}$</span>
-        <XCircleIcon className='h6 w-6 text-violet-700' ></XCircleIcon>
+        <XCircleIcon onClick={() => handleDelete(id)} className='h6 w-6 text-red-900' ></XCircleIcon>
       </div>
     </div>
   )
 };
 
 OrderCard.propTypes = {
+  id: PropTypes.string,
   image: PropTypes.string,
   title: PropTypes.string,
   price: PropTypes.number,
+  handleDelete: PropTypes.bool,
 }
 
 export default OrderCard;
